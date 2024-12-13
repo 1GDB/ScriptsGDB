2af5bc155571fbe964b6ad9e3e0681fad7daa383
GDB <3333@eclisboa.net>
2024-12-13 02:32:21 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 // Serve the Tampermonkey script
 app.get('/tampermonkey-script.js', (req, res) => {
-    res.sendFile(__dirname + '/tampermonkey-script.js');
+    res.sendFile(__dirname + '/Gestor_de_Construcoes-GDB.js');
 });
 
 // Example API endpoint for key verification
